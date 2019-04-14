04ea2fb3b901942aad9cdd34be9873080d966c1b
Marzena <marzena.szopinska090@gmail.com>
2019-04-14 13:29:21 +0200

    added file that containes data for the gallery

---
+++ src/galleryData.js
@@ -0,0 +1,38 @@
+const data = [
+  {
+    id: 01,
+    title: 'Vccusantium',
+    image: './images/01.jpg',
+    description:'Voluptatem accusantium doloremque laudantium,totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    id: 02,
+    title: 'Rccusantium',
+    image: './images/02.jpg',
+    description:'Roluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    id: 03,
+    title: 'Nccusantium',
+    image: './images/03.jpg',
+    description:'Noluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    id: 04,
+    title: 'Occusantium',
+    image: './images/04.jpg',
+    description:'Ooluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    id: 05,
+    title: 'Pccusantium',
+    image: './images/05.jpg',
+    description: 'Poluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    id: 06,
+    title: 'Iccusantium',
+    image: './images/06.jpg',
+    description:'Ioluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+];
